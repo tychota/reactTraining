7ab1a0e8b353d003f6a53a978e1a15896fa01f62
teseo <sol@javiermellado.com>
2018-02-12 09:54:57 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+// @flow
+
+import { AppRegistry } from 'react-native';
+import App from 'reactTraining/src/App';
+
+AppRegistry.registerComponent('reactTraining', () => App);
---
+++ src/RootNavigation.js
@@ -0,0 +1,35 @@
+// @flow
+
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { StackNavigator, addNavigationHelpers } from 'react-navigation';
+
+import * as Pages from 'reactTraining/src/pages';
+
+export const AppNavigator = StackNavigator({
+  home: {
+    screen: Pages.Home,
+  },
+});
+
+class App extends React.Component {
+  render() {
+    return (
+      <AppNavigator
+        navigation={addNavigationHelpers({
+          dispatch: this.props.dispatch,
+          state: this.props.nav,
+        })}
+      />
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  nav: state.nav,
+});
+
+const AppWithNavigationState = connect(mapStateToProps)(App);
+
+export default AppWithNavigationState;
